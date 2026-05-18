45a8896a26c891d346288e240cc61d3b07efe03c
ShiYuan <jw520qqwwee@gmail.com>
2026-05-18 11:09:28 +0800

    add third_party

---
+++ docker/third_party/install_dev.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+sudo apt install -y \
+  ninja-build \
+  libeigen3-dev \
+  libgflags-dev \
+  libgoogle-glog-dev \
+  libgtest-dev \
+  libgoogle-perftools-dev \
+  nlohmann-json3-dev \
+  linuxptp \
+  ptpd 
+
+# qtcreator chinese fonts
+sudo apt install -y \
+  fonts-noto-cjk \
+  qtmultimedia5-dev \
+  libqt5multimedia5-plugins
+
+sudo apt install -y \
+  gstreamer1.0-libav \
+  gstreamer1.0-plugins-base \
+  gstreamer1.0-plugins-good \
+  gstreamer1.0-plugins-bad \
+  gstreamer1.0-plugins-ugly \
+  gstreamer1.0-tools \
+  gstreamer1.0-plugins-base-apps
---
+++ third_party/Sophus/sophus.md
@@ -0,0 +1,9 @@
+## Sophus
+
+CodexOpen 使用的 sophus 库为简易基础版，修改自 **robosense_ac_slam**；
+
+详细代码：[robosense_ac_slam](https://github.com/RoboSense-Robotics/robosense_ac_slam) 
+
+## TODO
+
+更新为主流版本的 Sophus 库；
