19c30df6d34a347837ee7ebddd3347df1df3a7e2
ShiYuan <jw520qqwwee@gmail.com>
2026-08-15 23:23:33 +0800

    ai t_common_utils

--- tools/common/utils/rclcpp_utils.h
+++ tools/common/utils/rclcpp_utils.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 #include <rclcpp/rclcpp.hpp>
 
 namespace jojo {
@@ -8,6 +10,8 @@ namespace tools {
 uint64_t toMs(const builtin_interfaces::msg::Time& t);
 
 double toSec(const builtin_interfaces::msg::Time& t);
+
+rclcpp::Time fromMs(uint64_t milliseconds);
 
 }  // namespace tools
 }  // namespace jojo
